62f872bb4dd7d6d9ac63a9007115395dcab5b4f8
Tahicodia <rozhman.d.y@gmail.com>
2026-03-04 18:03:12 +0200

    Fixed Foldes.cpp

--- lab2/Folders.cpp
+++ lab2/Folders.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-Folders::Folders() : Folders("", "", "") {}
+Folders::Folders() : Folders("Homeworks", "04.03.2026", "Admin") {}
 Folders::Folders(string name, string date, string user) : name(name), date(date), user(user) {}
 Folders::Folders() { cout << "Folder destroyed/n"; }
 void Folders::Folders() const { cout << "Name: " << name << ", Date: " << date << ", User: " << user << endl }
