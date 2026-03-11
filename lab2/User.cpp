f78a59757eba527f5e6446e9ed12df3dcc0575ed
Tahicodia <rozhman.d.y@gmail.com>
2026-03-11 19:58:39 +0200

    add copy, move constructors, operators, static, etc.

--- lab2/User.cpp
+++ lab2/User.cpp
@@ -2,12 +2,46 @@
 #include <iostream>
 
 using namespace std;
+int User::userCount = 0;
+
 
 User::User(string name, string course, string group)
     : name(name), course(course), group(group) {
 }
 
 User::User() : User("Damir Rozhman", "Program Ingeneering", "143a") {}
+
+User::User(const User& other)
+    : name(other.name), course(other.course), group(other.group) {
+    cout << "User copied\n";
+}
+
+User::User(User&& other)
+    : name(move(other.name)), course(other.course), group(other.group) {
+    cout << "User moved\n";
+}
+
+void User::setGroup(string group) {
+    this->group = group;
+}
+
+int User::getCount() {
+    return userCount;
+}
+
+User User::operator+(const User& other) {
+    return User(name + "&" + other.name, course, group);
+}
+
+ostream& operator<<(ostream& os, const User& u) {
+    os << u.name << " " << u.course << " " << u.group;
+    return os;
+}
+
+istream& operator>>(istream& is, User& u) {
+    is >> u.name >> u.course >> u.group;
+    return is;
+}
 
 User::~User() {
     cout << "User destroyed\n";
--- lab2/User.h
+++ lab2/User.h
@@ -1,5 +1,6 @@
 #ifndef USER_H
 #define USER_H
+#include <iostream>
 #include <string>
 using namespace std;
 class User {
@@ -7,10 +8,18 @@ private:
 	string name;
 	string course;
 	string group;
+	static int userCount;
 public:
 	User();
 	User(string name, string course, string group);
+	User(const User& other);
+	User(User&& other);
 	~User();
-	void display() const;
+    void display() const;
+    void setGroup(string group);
+	static int getCount();
+	User operator+(const User& other);
+	friend ostream& operator<<(ostream& os, const User& s);
+	friend istream& operator>>(istream& is, User& s);
 };
 #endif
