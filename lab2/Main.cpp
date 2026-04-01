0e199034b90fe0b9ca99d5b0070040262fc801e3
Tahicodia <rozhman.d.y@gmail.com>
2026-04-01 20:29:01 +0300

    Fix problems in Main.cpp

--- lab2/Main.cpp
+++ lab2/Main.cpp
@@ -1,30 +1,21 @@
 #include "Folders.h"
 #include "Files.h"
 #include "User.h"
+#include <iostream>
+
 using namespace std;
 
 int main() {
-	Folders f1;
-	Folders f2("Homework2", "03.03.2026", "Student");
+    User u1;
+    User u2("Igor Aheienko", "Software Engineering", "143a");
+    Admin a1("Damir Rozhman", "Software Engineering", "143a", "System Moderator");
+    Files fi2("Lab_OOP", ".cpp", 20);
+    Folders f2("Project", "01.04.2026", "Damir", &fi2);
+    cout << "Total Users: " << User::getCount() << endl;
 
-	Files fi1;
-	Files fi2("Homework_OOP2", "Homework2", "Student");
+    u2.display(); cout << endl;
+    a1.display(); // Тут виведеться роль адміна
+    f2.display();
 
-	User u1;
-	User u2("Igor Aheienko", "Program Ingeneering", "143a");
-
-	User u3 = u2;
-	User u4 = u1 + u2;
-	cout << User::getCount() << endl;
-
-	f1.display();
-	f2.display();
-
-	fi1.display();
-	fi2.display();
-
-	u1.display();
-	u2.display();
-
-	return 0;
+    return 0;
 }
