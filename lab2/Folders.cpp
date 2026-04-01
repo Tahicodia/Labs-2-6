b571f97c2804089a5a82611d71d053c65643a71f
Tahicodia <rozhman.d.y@gmail.com>
2026-04-01 17:52:20 +0300

    Lab4 add Folders class using composition with Files

--- lab2/Folders.cpp
+++ lab2/Folders.cpp
@@ -1,18 +1,69 @@
 #include "Folders.h"
 #include <iostream>
-
 using namespace std;
 
+Folders::Folders() : name("Homeworks"), date("04.03.2026"), user("Admin"), files(nullptr) {}
+
 Folders::Folders(string name, string date, string user)
-    : name(name), date(date), user(user) {
+    : name(name), date(date), user(user), files(nullptr) {
 }
 
-Folders::Folders() : Folders("Homeworks", "04.03.2026", "Admin") {}
+Folders::Folders(string name, string date, string user, Files* files)
+    : name(name), date(date), user(user) {
+    if (files) this->files = new Files(*files); // Глибоке копіювання
+    else this->files = nullptr;
+}
+
+Folders::Folders(const Folders& other)
+    : name(other.name), date(other.date), user(other.user) {
+    if (other.files) this->files = new Files(*other.files);
+    else this->files = nullptr;
+}
+
+Folders::Folders(Folders&& other) noexcept
+    : name(move(other.name)),
+    date(move(other.date)),
+    user(move(other.user)) {
+
+    this->files = other.files; // Просто копіюємо адресу вказівника
+    other.files = nullptr;     // ВАЖЛИВО: зануляємо у старого об'єкта
+
+    cout << "Folder moved" << endl;
+}
+
+Folders& Folders::operator=(const Folders& other) {
+    if (this != &other) {
+        delete this->files; // Очищення старої пам'яті
+
+        this->name = other.name;
+        this->date = other.date;
+        this->user = other.user;
+
+        if (other.files) this->files = new Files(*other.files);
+        else this->files = nullptr;
+    }
+    return *this;
+}
 
 Folders::~Folders() {
-    cout << "Folder destroyed\n";
+    delete files;
+    cout << "Folder destroyed" << endl;
 }
 
 void Folders::display() const {
     cout << "Name: " << name << ", Date: " << date << ", User: " << user << endl;
+    if (files) {
+        cout << "  File: ";
+        files->display();
+    }
+}
+
+ZipFolder::ZipFolder(string name, string date, string user,
+    Files* files, double compressionRatio)
+    : Folders(name, date, user, files), compressionRatio(compressionRatio) {
+}
+
+void ZipFolder::display() const {
+    Folders::display(); // Виклик методу батька
+    cout << "  Compression: " << compressionRatio << "x" << endl;
 }
--- lab2/Folders.h
+++ lab2/Folders.h
@@ -1,16 +1,38 @@
 #ifndef FOLDERS_H
 #define FOLDERS_H
 #include <string>
+#include "Files.h"
 using namespace std;
+
 class Folders {
-	private:
-		string name;
-		string date;
-		string user;
-	public:
-		Folders();
-		Folders(string name, string date, string user);
-		~Folders();
-		void display() const;
+private:
+    string name;
+    string date;
+    string user;
+
+    Files* files;
+
+public:
+    Folders();
+    Folders(string name, string date, string user);
+    Folders(string name, string date, string user, Files* files);
+
+    Folders(const Folders& other);
+    Folders(Folders&& other) noexcept;
+    Folders& operator=(const Folders& other);
+
+    virtual ~Folders();
+    virtual void display() const;
+};
+
+class ZipFolder : public Folders {
+private:
+    double compressionRatio;
+
+public:
+    ZipFolder(string name, string date, string user,
+        Files* files, double compressionRatio);
+
+    void display() const override;
 };
 #endif
